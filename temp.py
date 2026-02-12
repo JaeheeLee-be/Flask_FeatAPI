89545afd27bc46090d04ad92de5bd8ccfea2c1cd
JaeheeLee-be <clodusky1@gmail.com>
2026-02-12 17:43:14 +0900

    commit

--- temp.py
+++ temp.py
@@ -3,10 +3,10 @@ def add(a: int, b: int) -> int:
 
 
 def test_add() -> None:
-    # Given
+    # Given: 재료 준비
     a, b = 1, 1
 
-    # When
+    # When: 테스트 대상이 되는 함수를 호출합니다.
     result = add(a, b)
 
     # Then
